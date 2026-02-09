10b96ec85ccdbf460bbe5d6f74a7d06cf718c362
Jean Jadot <j.jadot@helmo.be>
2026-02-09 12:44:08 +0100

    add NotWorkingPage + css files

---
+++ BioDomes.Web/Pages/NotWorkingPage.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BioDomes.Web.Pages;
+
+public class NotWorkingPage : PageModel
+{
+    public void OnGet()
+    {
+        throw new InvalidOperationException("Congrats ! You reached the not working page in dev mode");
+    }
+}
