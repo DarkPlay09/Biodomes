38deba0fb97c9c45aaf8922fa3d002d56d449b3a
Loick Carlier <loi.carlier@student.helmo.be>
2026-03-09 19:20:29 +0100

    Labo 03 terminé: slug route + constraint + transformer

---
+++ BioDomes.Domains.Tests/StringExtensionsTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+using BioDomes.Domains;
+
+public class StringExtensionsTests
+{
+    [TestCase("Panthera leo", "panthera-leo")]
+    [TestCase("Equus quagga", "equus-quagga")]
+    [TestCase("Quercus acerifolia", "quercus-acerifolia")]
+    public void ToKebabCase_Works(string input, string expected)
+    {
+        Assert.That(input.ToKebabCase(), Is.EqualTo(expected));
+    }
+}
