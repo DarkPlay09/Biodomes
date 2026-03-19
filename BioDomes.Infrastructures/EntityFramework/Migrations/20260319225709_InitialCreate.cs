d8fab4ad02c7b38b986e9606e6b76d5e1530a39b
Loick Carlier <loi.carlier@student.helmo.be>
2026-03-19 23:58:27 +0100

    Fix database

--- BioDomes.Infrastructures/EntityFramework/Migrations/20260319225709_InitialCreate.cs
+++ BioDomes.Infrastructures/EntityFramework/Migrations/20260319225709_InitialCreate.cs
@@ -2,6 +2,8 @@
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace BioDomes.Infrastructures.EntityFramework.Migrations
 {
     /// <inheritdoc />
@@ -28,6 +30,23 @@ namespace BioDomes.Infrastructures.EntityFramework.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Species", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Species",
+                columns: new[] { "Id", "AdultSize", "Classification", "CreatedByUserName", "Diet", "ImageUrl", "IsPublic", "Name", "Weight" },
+                values: new object[,]
+                {
+                    { 1, 2.5, "Mammifère", "admin", "Carnivore", null, true, "Lion d'Afrique", 190.0 },
+                    { 2, 9.0, "Plante", "admin", "Photosynthèse", null, true, "Monstera", 12.0 },
+                    { 3, 0.94999999999999996, "Oiseau", "admin", "Herbivore", null, true, "Ara Rouge", 1.2 },
+                    { 4, 1.5, "Reptile", "admin", "Herbivore", null, true, "Tortue Géante", 250.0 },
+                    { 5, 0.46000000000000002, "Mammifère", "admin", "Omnivore", null, true, "Lémur Catta", 2.2000000000000002 },
+                    { 6, 85.0, "Plante", "admin", "Photosynthèse", null, true, "Séquoia Géant", 1200000.0 },
+                    { 7, 0.80000000000000004, "Plante", "admin", "Photosynthèse", null, true, "Aloe vera", 15.0 },
+                    { 8, 1.6000000000000001, "Mammifère", "admin", "Carnivore", null, true, "Loup gris", 45.0 },
+                    { 9, 0.59999999999999998, "Mammifère", "admin", "Omnivore", null, true, "Raton laveur", 9.0 },
+                    { 10, 2.2000000000000002, "Reptile", "admin", "Carnivore", null, true, "Boa constrictor", 13.0 }
                 });
         }
 
